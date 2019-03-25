3b6cb96d7019053ca272ccb2a8d104cfe826213b
Diana Barsan <barsan@medicmobile.org>
2019-03-25 19:25:19 +0200

    Revert exception-handler

--- webapp/src/js/services/exception-handler.js
+++ webapp/src/js/services/exception-handler.js
@@ -15,11 +15,11 @@
 
       var error = { message: exception.message, stack: exception.stack, cause: cause };
       try {
-        /*Feedback.submit(error, false, function(err) {
+        Feedback.submit(error, false, function(err) {
           if (err) {
             $log.error('Error saving feedback', err);
           }
-        });*/
+        });
       } catch(e) {
         // stop infinite loop of exceptions
         $log.error('Error while trying to record error', JSON.stringify(error), e.toString(), e);
